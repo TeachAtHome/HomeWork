9435cfe6606c39d9d857ebbc5af55f1a323f207a
Loewe1000 <loewe1000@googlemail.com>
2020-03-21 19:29:27 +0100

    Grades List

--- frontend/teachathome-frontend/client/src/dashboard/components/Grades/Grades.js
+++ frontend/teachathome-frontend/client/src/dashboard/components/Grades/Grades.js
@@ -1,71 +1,46 @@
-import React, { Component } from 'react';
-import './Grades.css';
+import React, { Component } from "react";
+import "./Grades.css";
+import ornament from '../../assets/math.svg';
 
 export default class Grades extends Component {
   render() {
     return (
-      <div id="Kranke_Sch_ler_">
-        <svg className="Surface_">
-          <rect
-            fill="rgba(255,255,255,1)"
-            id="Surface_"
-            rx="4"
-            ry="4"
-            x="0"
-            y="0"
-            width="229.445"
-            height="470"
-          ></rect>
-        </svg>
-        <div id="Klassen">
+      <div className="Container">
+        <svg className="Shadow"></svg>
+        <div className="Headline">
           <span>Klassen</span>
         </div>
-        <div id="Repeat_Grid_3">
-          <div id="Repeat_Grid_3_0">
-            <div id="Klasse_8a">
-              <span>Klasse 8a</span>
-            </div>
-          </div>
-          <div id="Repeat_Grid_3_1">
-            <div id="Klasse_8a_ba">
-              <span>Klasse 8a</span>
-            </div>
-          </div>
-          <div id="Repeat_Grid_3_2">
-            <div id="Klasse_8a_bc">
-              <span>Klasse 8a</span>
-            </div>
-          </div>
-          <div id="Repeat_Grid_3_3">
-            <div id="Klasse_8a_be">
-              <span>Klasse 8a</span>
-            </div>
-          </div>
-          <div id="Repeat_Grid_3_4">
-            <div id="Klasse_8a_bg">
-              <span>Klasse 8a</span>
-            </div>
-          </div>
-          <div id="Repeat_Grid_3_5">
-            <div id="Klasse_8a_bi">
-              <span>Klasse 8a</span>
-            </div>
-          </div>
-          <div id="Repeat_Grid_3_6">
-            <div id="Klasse_8a_bk">
-              <span>Klasse 8a</span>
-            </div>
-          </div>
-          <div id="Repeat_Grid_3_7">
-            <div id="Klasse_8a_bm">
-              <span>Klasse 8a</span>
-            </div>
-          </div>
-          <div id="Repeat_Grid_3_8">
-            <div id="Klasse_8a_bo">
-              <span>Klasse 8a</span>
-            </div>
-          </div>
+        <div className="Entries">
+        <div className="Entry">
+          <img src={ornament} className="Icon"/>
+          <div className="Label">Mathematik</div>
+          <div className="Label">8a</div>
+        </div>
+        <div className="Entry">
+          <img src={ornament} className="Icon"/>
+          <div className="Label">Mathematik</div>
+          <div className="Label">8a</div>
+        </div>
+        <div className="Entry">
+          <img src={ornament} className="Icon"/>
+          <div className="Label">Mathematik</div>
+          <div className="Label">8a</div>
+        </div>
+        <div className="Entry">
+          <img src={ornament} className="Icon"/>
+          <div className="Label">Mathematik</div>
+          <div className="Label">8a</div>
+        </div>
+        <div className="Entry">
+          <img src={ornament} className="Icon"/>
+          <div className="Label">Mathematik</div>
+          <div className="Label">8a</div>
+        </div>
+        <div className="Entry">
+          <img src={ornament} className="Icon"/>
+          <div className="Label">Mathematik</div>
+          <div className="Label">8a</div>
+        </div>
         </div>
       </div>
     );

f1873ccb4c3feb24658fae61b22ca8baf0625aef
Kelvin Glaß <kelvin.glass@fu-berlin.de>
2020-03-21 18:28:13 +0100

    Add editor

--- frontend/teachathome-frontend/client/src/routes.js
+++ frontend/teachathome-frontend/client/src/routes.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Router, Route} from 'react-router';
 import TeachAtHome from './routes/TeachAtHome';
 import Classroom from './routes/Classroom';
+import EditorRouting from './routes/EditorRouting';
 
 const Routes = (props) => (
   <Router {...props}>
     <Route path="/" component={TeachAtHome} />
+    <Route path="/editor" component={EditorRouting} />
     <Route path="/classroom" component={Classroom} />
   </Router>
 );

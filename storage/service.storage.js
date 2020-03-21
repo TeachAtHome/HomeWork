ba5d127ed23ca9beeb53cd5628a4fa2c9336af76
Chwalek, Kevin <kevin.chwalek@gmx.de>
2020-03-21 16:31:43 +0100

    feat(storage): fix async upload function

--- storage/service.storage.js
+++ storage/service.storage.js
@@ -5,27 +5,21 @@ class StorageService {
         this.storage = new Storage({ projectId: 'ninth-moment-271720', keyFilename: "key.json" });
     }
 
-    uploadDocument(document) {
+    async uploadDocument(document) {
         console.log('StorageService|uploadDocument');
-        async function uploadToGCS(filename) {
-            // Uploads a local file to the bucket
-            await storage.bucket('teachathome-document-bucket').upload(filename, {
-                // Support for HTTP requests made with `Accept-Encoding: gzip`
-                gzip: true,
-                // By setting the option `destination`, you can change the name of the
-                // object you are uploading to a bucket.
-                metadata: {
-                    // Enable long-lived HTTP caching headers
-                    // Use only if the contents of the file will never change
-                    // (If the contents will change, use cacheControl: 'no-cache')
-                    cacheControl: 'public, max-age=31536000',
-                },
-            });
-
-            console.log(`${filename} uploaded to ${bucketName}.`);
-        }
-
-        uploadToGCS(document.tempFilePath).catch(console.error);
+        await this.storage.bucket('teachathome-document-bucket').upload(document.tempFilePath, {
+            // Support for HTTP requests made with `Accept-Encoding: gzip`
+            gzip: true,
+            // By setting the option `destination`, you can change the name of the
+            // object you are uploading to a bucket.
+            metadata: {
+                // Enable long-lived HTTP caching headers
+                // Use only if the contents of the file will never change
+                // (If the contents will change, use cacheControl: 'no-cache')
+                cacheControl: 'public, max-age=31536000',
+            },
+        });
+        console.log(`${document.tempFilePath} uploaded to teachathome-document-bucket.`);
     }
 
 }

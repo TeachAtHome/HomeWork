4b6913c9cd871da36944949b62debfd09a732440
Pascal Horninge <>
2020-03-21 17:51:43 +0100

    added new classroom page

---
+++ frontend/teachathome-frontend/client/src/components/classroom/Classroom.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import "./Classroom.css";
+import Studentlist from "./Studentlist";
+
+
+
+export default class Classroom extends Component {
+  state = {
+    response: "",
+    post: "",
+    responseToPost: "",
+    classroom: {}
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ classroom: res }))
+      .catch(err => console.log(err));
+    console.log(this.state);
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/group/3B-Mathe");
+    console.log(response);
+    const className = await response.json();
+    console.log(className);
+    if (response.status !== 200) throw Error(className.message);
+    return className;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <a className="App-link" href="/" rel="noopener noreferrer">
+            Startseite
+          </a>
+        </header>
+        <span>{this.state.classroom.name}</span>
+        <div>
+          <Studentlist />
+        </div>
+        <a href="#" onClick={console.log("TEST")}> New Entry</a>
+      </div>
+    );
+  }
+}
---
+++ frontend/teachathome-frontend/client/src/routes/Classroom.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Classroom from '../components/classroom/Classroom';
+
+class TeachAtHome extends Component {
+  render() {
+    return (
+      <div>
+        <Classroom />
+      </div>
+    );
+  }
+}
+
+export default TeachAtHome;

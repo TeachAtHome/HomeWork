ff3de0d596897707d86e733b8ac4177bdcc88927
lucaspalomodevelop <lucas.palomo@t-online.de>
2020-03-21 01:07:45 +0100

    add schueler and lehrer routes

---
+++ routes/lehrer/index.js
@@ -0,0 +1,13 @@
+//import modules
+var express = require('express');
+var router = express.Router();
+
+/* GET admin page. */
+router.get('/lehrer/:id', function(req, res, next){
+    res.send("lehrer");
+});
+
+
+module.exports = router; 
+
+

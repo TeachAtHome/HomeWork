8ebec48d8d507b68691ae4252ffa33ceb7af1e59
Chwalek, Kevin <kevin.chwalek@accenture.com>
2020-03-21 01:07:24 +0100

    feat(document): add upload function

--- app.js
+++ app.js
@@ -1,10 +1,54 @@
 var express = require('express');
 var app = express();
 
+const cors = require('cors');
+app.use(cors());
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// fileupload
+const fileUpload = require('express-fileupload');
+app.use(fileUpload({
+  createParentPath: true,
+  safeFileNames: true,
+  useTempFiles: true
+}));
+
+// const {initBucket} = require('./storage/gcsService');
+// initBucket();
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+const { uploadDocument } = require('./storage/gcsService');
+
+app.put('/upload', function (req, res) {
+  try {
+    if (!req.files || !req.files.document) {
+      res.status(400).send({
+        status: false,
+        message: 'No file uploaded'
+      });
+    } else {
+      uploadDocument(req.files.document);
+      res.send({
+        status: true,
+        message: 'File is uploaded',
+        // data: {
+        //   name: avatar.name,
+        //   mimetype: avatar.mimetype,
+        //   size: avatar.size
+        // }
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.listen(8000, function () {
+  console.log('Example app listening on port 8000!');
 });

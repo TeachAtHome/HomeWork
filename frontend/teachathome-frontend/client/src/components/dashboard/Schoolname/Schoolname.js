4f5b60d01bb82483ef043659378a5a39bcc06caa
Pascal Horninge <>
2020-03-22 10:53:32 +0100

    added Project Logo

--- frontend/teachathome-frontend/client/src/components/dashboard/Schoolname/Schoolname.js
+++ frontend/teachathome-frontend/client/src/components/dashboard/Schoolname/Schoolname.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import './Schoolname.css';
+import logo from '../../../assets/Logo.png';
+import logoText from '../../../assets/LogoText.png';
 
 export default class Schoolname extends Component {
   render() {
     return (
       <div id="Container">
+        <a href="/">
+          <img id="Logo" src={logo} alt="Logo" />
+          <img id="LogoText" src={logoText} alt="logoText" />
+        </a>
         <div id="Titel">
           <span>Geschwister-Scholl-Gymnasium</span>
         </div>
